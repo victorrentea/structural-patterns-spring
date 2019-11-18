1f3387f9b2adfc74ee554b595e072365c41c4b42
Victor Rentea <victorrentea@gmail.com>
2019-11-18 20:35:47 +0200

    Proxy: Interface Proxies take 1

--- src/main/java/victor/training/oo/structural/proxy/LetsPlay.java
+++ src/main/java/victor/training/oo/structural/proxy/LetsPlay.java
@@ -1,4 +1,48 @@
 package victor.training.oo.structural.proxy;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.Arrays;
+
 public class LetsPlay {
+
+    public static void main(String[] args) {
+        MathematicsImpl realImpl = new MathematicsImpl();
+
+        InvocationHandler h = new InvocationHandler() {
+            @Override
+            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                System.out.println("CIA here: You are calling : " + method.getName() +
+                        " with args " + Arrays.toString(args));
+                return method.invoke(realImpl, args);
+            }
+        };
+
+        Mathematics m = (Mathematics) Proxy.newProxyInstance(LetsPlay.class.getClassLoader(),
+                new Class<?>[]{Mathematics.class},
+                h);
+
+        bizLogic(m);
+    }
+
+    private static void bizLogic(Mathematics m) {
+        System.out.println("Who's on the phone? " + m.getClass());
+        System.out.println(m.sum(1,1));
+        System.out.println(m.sum(2,0));
+        System.out.println(m.sum(3,-1));
+        System.out.println(m.sum(3,1));
+    }
+}
+
+
+interface Mathematics {
+    int sum(int a, int b);
+}
+class MathematicsImpl implements Mathematics {
+
+    @Override
+    public int sum(int a, int b) {
+        return a + b;
+    }
 }
