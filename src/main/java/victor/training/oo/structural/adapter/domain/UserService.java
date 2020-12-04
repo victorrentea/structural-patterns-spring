88decdc15b358b95c71d57a782f1ca8abd0034bc
Victor Rentea <victorrentea@gmail.com>
2020-12-04 15:41:51 +0200

    extracted duplication

--- src/main/java/victor/training/oo/structural/adapter/domain/UserService.java
+++ src/main/java/victor/training/oo/structural/adapter/domain/UserService.java
@@ -1,5 +1,6 @@
 package victor.training.oo.structural.adapter.domain;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,34 +12,45 @@ import java.util.List;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
+// Domain Service
+// PEace, harmony, ZEN
 public class UserService {
-	@Autowired
-	private LdapUserWebserviceClient wsClient;
+	private final LdapUserWebserviceClient wsClient;
 
 	public void importUserFromLdap(String username) {
-		List<LdapUser> list = wsClient.search(username.toUpperCase(), null, null);
+		List<LdapUser> list = searchByUsername(username);
 		if (list.size() != 1) {
 			throw new IllegalArgumentException("There is no single user matching username " + username);
 		}
 		LdapUser ldapUser = list.get(0);
-		String fullName = ldapUser.getfName() + " " + ldapUser.getlName().toUpperCase();
-		User user = new User(ldapUser.getuId(), fullName, ldapUser.getWorkEmail());
-		
+		User user = convert(ldapUser);
+
 		if (user.getWorkEmail() != null) {
 			log.debug("Send welcome email to " + user.getWorkEmail());
 		}
 		log.debug("Insert user in my database");
 	}
-	
+
+	//200 lines below
 	public List<User> searchUserInLdap(String username) {
-		List<LdapUser> list = wsClient.search(username.toUpperCase(), null, null);
+		List<LdapUser> list = searchByUsername(username);
 		List<User> results = new ArrayList<>();
 		for (LdapUser ldapUser : list) {
-			String fullName = ldapUser.getfName() + " " + ldapUser.getlName().toUpperCase();
-			User user = new User(ldapUser.getuId(), fullName, ldapUser.getWorkEmail());
-			results.add(user);
+			results.add(convert(ldapUser));
 		}
 		return results;
 	}
-	
+
+	/// extracted duplication
+
+	private User convert(LdapUser ldapUser) {
+		String fullName = ldapUser.getfName() + " " + ldapUser.getlName().toUpperCase();
+		return new User(ldapUser.getuId(), fullName, ldapUser.getWorkEmail());
+	}
+
+	private List<LdapUser> searchByUsername(String username) {
+		return wsClient.search(username.toUpperCase(), null, null);
+	}
+
 }
